895cd187fc5103be8d65fda6ebd67bf75d13e9d1
Guatapickl <robert.washko@gmail.com>
2025-07-11 09:39:52 -0400

    style: adjust layout wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import ChatWindow from '@/components/ChatWindow';
 
 export default function Home() {
   return (
-    <div className="relative w-full max-w-6xl mx-auto">
-      <div className="flex justify-center relative z-10">
+    <div className="relative w-full max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex justify-center relative z-10 px-6 sm:px-12 lg:px-20">
         <ChatWindow />
       </div>
     </div>
