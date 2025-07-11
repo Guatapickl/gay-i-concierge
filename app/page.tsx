7f4b9ecc9c4bda2380d6d54abd0e1946a338bffc
Guatapickl <robert.washko@gmail.com>
2025-07-10 20:21:53 -0400

    chore: constrain chat page container

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ChatWindow from '@/components/ChatWindow';
 
 export default function Home() {
   return (
-    <div className="relative">
+    <div className="relative w-full max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex justify-center relative z-10">
         <ChatWindow />
       </div>
